f4ebdf29f0d320197da81f789de4694357d65c1c
Roman Chyla <roman.chyla@gmail.com>
2016-12-07 20:49:24 -0500

    Updates

--- measure-bandwith-4x.sh
+++ measure-bandwith-4x.sh
@@ -6,6 +6,4 @@ source ./common.sh
 target=4x
 mkdir -p $SOLRJMETER_HOME/$target
 
-for qfile in $(ls . ); do
-    python $homedir/solrjmeter.py -a -x $homedir/jmx/SolrQueryMeasureBandwidth.jmx -q foo -s $SERVER -p $PORT --durationInSecs $DURATION -R $target -t $TARGET_URL -e $COLLECTION
-done
+python $homedir/solrjmeter.py -a -x $homedir/jmx/SolrQueryMeasureBandwidth.jmx -q queries/adsabs/LowFreqTerms -s $SERVER -p $PORT --durationInSecs $DURATION -R $target -t $TARGET_URL -e $COLLECTION
